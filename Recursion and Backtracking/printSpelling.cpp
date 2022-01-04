62ca1c92beb1cf0dd547691aeff2379589dfab4a
gsharma101 <sharmag8121@gmail.com>
2022-01-04 23:56:13 +0530

    recursion practice

--- Recursion and Backtracking/printSpelling.cpp
+++ Recursion and Backtracking/printSpelling.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 // This program illustrate using recursion to solve febonacci series
 int series(int n){
-   if( n == 0 | n == 1){
-       return n;
-   }
-   return series(n-1) + series(n-2);
+  
 }
 
 int main(){
     int n;
     cout<<"Enter any number to calculate factorial\n";
     cin>>n;
-    cout<<"Value of "<<n<<" th Fibonacci is "<<series(n)<<"\n";
+    
 }
